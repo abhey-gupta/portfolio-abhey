128e3737d8dd3c6a9435be10d637454d8f8ed07b
abhey-gupta <abheygupta1731@gmail.com>
2024-03-27 16:39:42 +0530

    added footer

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
@@ -16,6 +17,7 @@ export default function Home() {
       <Skills />
       {/* <Experience /> */}
       <Contact />
+      <Footer />
     </main>
   );
 }
