d0ed7e19bc2c1e1a596edfabd0565082729c3aa9
abhey-gupta <abheygupta1731@gmail.com>
2024-03-27 16:57:12 +0530

    name fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Ricardo. All rights reserved.
+        &copy; 2024 Abhey. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
